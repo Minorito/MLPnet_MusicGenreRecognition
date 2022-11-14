d289b52820e5c8189e1f7beb89ccc36d8cfb8d42
Minorito <71348537+Minorito@users.noreply.github.com>
2022-11-14 09:08:06 -0300

    Update MLPnet.py

--- MLPnet.py
+++ MLPnet.py
@@ -33,19 +33,6 @@ from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers import RMSprop
 
-seed = 10
-np.random.seed(seed)
-import numpy as np
-import matplotlib.pyplot as plt
-from itertools import cycle
-
-from sklearn import svm, datasets
-from sklearn.metrics import roc_curve, auc
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import label_binarize
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import roc_auc_score
-
 from google.colab import drive
 drive.mount('/content/drive/')
 
